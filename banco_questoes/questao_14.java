7db086ed75701b2ed32929ada3ab3585ce95c068
caiodassumpcao <caio.rodri.d@gmail.com>
2024-10-22 14:34:04 -0300

    Reconfigurando pastas

--- banco_questoes/questao_14.java
+++ banco_questoes/questao_14.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class questao_14 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a areasta do cubo:");
+        double aresta = sc.nextDouble();
+        double volume = Math.pow(aresta, 3);
+        System.out.printf("Volume do cubo: %.3f", volume);
     }
 }
