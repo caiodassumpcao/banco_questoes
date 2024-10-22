1dae56337be490f98aca3da9c19bc8117008399f
caiodassumpcao <caio.rodri.d@gmail.com>
2024-10-22 15:07:40 -0300

    questao_18

---
+++ banco_questoes/questao_18.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class questao_18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Qual sua altura em metros?");
+        float altura = sc.nextFloat();
+
+        System.out.println("Qual seu gênero? (masculoni ou feminino)");
+        String genero = sc.next();
+
+        String homem = "masculino";
+        String mulher = "feminino";
+        
+        if (genero.equals(homem)) {
+            float peso_ideal = (float) (( altura * 72.7 ) - 58);
+            System.out.printf("O seu peso ideal em kg: %.3f", peso_ideal);
+
+        } else if (genero.equals(mulher)) {
+            float peso_ideal = (float) (( altura * 62.1 ) - 44.7);
+            System.out.printf("O seu peso ideal em kg: %.3f", peso_ideal);
+        } else {
+            System.out.println("Gênero inválido!");
+        } 
+     }
+}
