c526545b9354d57643d2e9266a0064d7e525de6d
caiodassumpcao <caio.rodri.d@gmail.com>
2024-10-24 17:27:07 -0300

    questao_23

---
+++ banco_questoes/questao_23.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class questao_23 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Qual tamanho do arquivo (em MB)? ");
+        double mb = sc.nextDouble();
+        System.out.println("Qual a velociade do link (em Mbps)? ");
+        double mbps = sc.nextDouble();
+
+        double tempo_s = mb / (mbps / 10);
+        float tempo_m = (float) (tempo_s / 60);
+        System.out.printf("A velocidade em segundos é: %.3f", tempo_m);
+
+        sc.close();
+    }
+}
