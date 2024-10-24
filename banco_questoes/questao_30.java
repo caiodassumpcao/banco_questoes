9e672343a8c7b89a00a8a31e2bb1269c9cbccd34
caiodassumpcao <caio.rodri.d@gmail.com>
2024-10-24 18:06:58 -0300

    questao_30

---
+++ banco_questoes/questao_30.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class questao_30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nome do vendedor:");
+        String nome = sc.nextLine();
+        
+        System.out.println("Salário fixo:");
+        double salario_fixo = sc.nextDouble();
+        System.out.println("Total de vendas no mês (em R$):");
+        double vendas = sc.nextDouble();
+        
+        double comissao = vendas * 0.15;
+        
+        double salario_total = salario_fixo + comissao;
+        
+        System.out.println("Vendedor: " + nome + "\n");
+        System.out.printf("Salario fixo: R$ %.3f\n", salario_fixo);
+        System.out.printf("Comissão de vendas: R$ %.3f\n", comissao);
+        System.out.printf("Salário total: R$ %.3f\n", salario_total);
+        
+
+
+        sc.close();
+
+    }
+}
