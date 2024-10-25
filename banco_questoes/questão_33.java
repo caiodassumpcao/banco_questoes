cbd94c77feb690164fa7cc69ad9d1c3928b398e6
caiodassumpcao <caio.rodri.d@gmail.com>
2024-10-25 09:21:35 -0300

    Questão 33

---
+++ banco_questoes/questão_33.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class questão_33 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o custo de fábrica do carro em reais:");
+        double custoFabrica = sc.nextDouble(); 
+
+        double imposto = custoFabrica * 1.45;
+        double custoFinal = imposto * 1.28;
+
+        System.out.printf("Custo ao consumidor: %.2f\n", custoFinal);
+        
+        System.out.printf("Custo de fábrica: %.2f\n", custoFabrica);
+        sc.close();
+    }
+}
