9dd5b73022399156646e3c6f826bd9c752a427e2
caiodassumpcao <caio.rodri.d@gmail.com>
2024-10-25 09:39:40 -0300

    Questão 34

---
+++ banco_questoes/questao_34.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class questao_34 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número menor que 1000:");
+        int num = sc.nextInt(); 
+        
+        if (num < 0 || num >= 1000) {
+            System.out.println("Número inválido. Digite o número novamente!");
+        } else {
+            int centenas = num / 100;
+            int dezenas = (num % 100) / 10;
+            int unidades = num % 10;
+
+            System.out.printf("Centenas = %d, Dezenas = %d, Unidades = %d\n", centenas, dezenas, unidades);
+
+        }
+        sc.close();
+    }
+}
