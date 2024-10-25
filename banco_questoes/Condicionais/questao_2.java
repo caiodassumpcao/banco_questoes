0bdea86ace3c5e3a846fb3fa5b5294fea1eb4467
caiodassumpcao <caio.rodri.d@gmail.com>
2024-10-25 10:06:12 -0300

    Questão 2

---
+++ banco_questoes/Condicionais/questao_2.java
@@ -0,0 +1,20 @@
+package Condicionais;
+
+import java.util.Scanner;
+
+public class questao_2 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.println("Digite um número positivo:");
+        double num_1 = sc.nextDouble();
+
+        sc.close();
+
+        double numeroOposto = num_1 * -1;
+
+        System.out.println("O oposto do número é: " + numeroOposto);
+    }
+}
