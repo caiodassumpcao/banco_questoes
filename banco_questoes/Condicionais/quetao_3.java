5eec6d114493e0c3544e91bcd27f535f09994ecc
caiodassumpcao <caio.rodri.d@gmail.com>
2024-10-25 10:43:39 -0300

    Questão 3

---
+++ banco_questoes/Condicionais/quetao_3.java
@@ -0,0 +1,26 @@
+package Condicionais;
+
+import java.util.Scanner;
+
+public class quetao_3 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Qual seu gênero?");
+        String gen = sc.next();
+
+        if (gen.charAt(0) == 'f' || gen.charAt(0) == 'F') {
+            System.out.println("Gênero Feminino");
+            
+        } else if (gen.charAt(0) == 'm' || gen.charAt(0) == 'M') {
+            System.out.println("Gênero Masculino");
+            
+        } else {
+            System.out.println("Gênero inválido");
+
+        }
+
+        sc.close();
+    }
+}
