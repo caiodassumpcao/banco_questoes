127cd88e312d74bf397250caf98ac3f82e34819b
caiodassumpcao <caio.rodri.d@gmail.com>
2024-10-25 12:00:03 -0300

    Questão 4

---
+++ banco_questoes/Condicionais/questao_4.java
@@ -0,0 +1,25 @@
+package Condicionais;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class questao_4 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite uma letra:");
+        String letra = sc.next().toLowerCase();
+
+        String[] vogais = {"a", "e", "i", "o", "u"};
+
+        if (Arrays.asList(vogais).contains(letra)) {
+            System.out.println("Letra é uma vogal");
+            
+        } else {
+            System.out.println("Letra é uma consoante");
+
+        }
+        sc.close();
+    }
+}
