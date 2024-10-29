4d246032d8ba2d2b6ef6e36fda00a4358758ba0f
caiodassumpcao <caio.rodri.d@gmail.com>
2024-10-29 12:16:12 -0300

    Modification Questão 5

--- banco_questoes/Condicionais/questao_5.java
+++ banco_questoes/Condicionais/questao_5.java
@@ -7,9 +7,21 @@ public class Questao_5 {
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Digite sua nota da AV1:");
+        double av1 = sc.nextDouble();
+        System.out.println("Digite sua nota da AV2:");
+        double av2 = sc.nextDouble();
         
+        double media = (av1 + av2) / 2;
+        
+        if (Math.abs(media - 10) < 0.0001) {
+            System.out.println("Aprovado com mérito");
+        } else if (media >= 7){
+            System.out.println("Aprovado por média");
+            
+        } else {
+            System.out.println("Reprovado");
+
+        }
         sc.close();
     }
-
-
 }
