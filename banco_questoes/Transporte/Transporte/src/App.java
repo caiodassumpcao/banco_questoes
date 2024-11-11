c61f39f9e6ccf3ee0b5531174ca029a8ea532b6a
caiodassumpcao <caio.rodri.d@gmail.com>
2024-11-11 18:59:02 -0300

    Questão 1: Classe transporte

---
+++ banco_questoes/Transporte/Transporte/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Transporte transporte = new Transporte(1, 2015, "Gol", 5000.0, 350.0);
+
+        transporte.exibirInformacoes();
+    }
+}
